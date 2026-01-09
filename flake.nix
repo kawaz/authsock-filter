15405d01a17543ef747e4a22030957e0cb402f12
Yoshiaki Kawazu <kawazzz@gmail.com>
2026-01-09 16:25:27 +0900

    Switch flake.nix to use pre-built binaries from GitHub Releases
    Instead of building from source with rustPlatform, now downloads
    pre-built binaries for faster installation. Supports:
    - x86_64-linux
    - x86_64-darwin
    - aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -7,21 +7,54 @@
 
   outputs = { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      version = "0.1.38";
+
+      # バイナリ配布があるプラットフォームのみ
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      # プラットフォームごとのアーカイブ情報
+      archives = {
+        x86_64-linux = {
+          url = "https://github.com/kawaz/authsock-filter/releases/download/v${version}/authsock-filter-x86_64-unknown-linux-gnu.tar.gz";
+          sha256 = "sha256-7HB90YLfOEaWVZfT9AemMo/MjgITVPSTjoCicoc0J/M=";
+        };
+        x86_64-darwin = {
+          url = "https://github.com/kawaz/authsock-filter/releases/download/v${version}/authsock-filter-x86_64-apple-darwin.tar.gz";
+          sha256 = "sha256-O7xO18p1TZ0CfbH+SgE2PdezPrR0hWKS9b2ctGG49Kk=";
+        };
+        aarch64-darwin = {
+          url = "https://github.com/kawaz/authsock-filter/releases/download/v${version}/authsock-filter-aarch64-apple-darwin.tar.gz";
+          sha256 = "sha256-s/7gXChMweYRzH1aFPaYOEIpQvRBmsPLB5G+wehrZiI=";
+        };
+      };
+
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          archive = archives.${system};
         in
         {
-          default = pkgs.rustPlatform.buildRustPackage {
+          default = pkgs.stdenv.mkDerivation {
             pname = "authsock-filter";
-            version = "0.1.38";
+            inherit version;
 
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.fetchurl {
+              url = archive.url;
+              sha256 = archive.sha256;
+            };
+
+            sourceRoot = ".";
+
+            nativeBuildInputs = [ pkgs.gzip ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp authsock-filter $out/bin/
+              chmod +x $out/bin/authsock-filter
+            '';
 
             meta = with pkgs.lib; {
               description = "SSH agent proxy with filtering and logging";
@@ -29,6 +62,7 @@
               license = licenses.mit;
               maintainers = [ ];
               mainProgram = "authsock-filter";
+              platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
             };
           };
         });
